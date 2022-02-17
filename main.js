24f92f46ecc2ce68f8e97e0e56d9a11eeb1f4c6d
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2022-02-17 15:23:58 +0500

    updated

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let API_KEY = 'dcea1fd7b3e65d34387ad6de7ef9cc5e'
 let tokenTop = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&page=1` 
 let tokenPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=1`
 let tokenUpComing = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&page=1`
-let local = JSON.parse(window.localStorage.getItem('last')) || ''
+let local = JSON.parse(window.localStorage.getItem('last')) || 'popular'
 
 start()
 
